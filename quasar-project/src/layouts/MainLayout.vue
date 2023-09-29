7e498d407a256b2c55b5836d861bf22a7c83f93b
Emmanuel <emmanuel.ribierre@live.fr>
2023-09-29 10:16:56 +0200

    initial commit

---
+++ quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated> </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> SU EAD Sciences </q-item-label>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {},
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-calendar
+    style="height:400px"></q-calendar>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {  },
+  setup () {
+
+
+    return {  };
+  }
+});
+</script>
+
